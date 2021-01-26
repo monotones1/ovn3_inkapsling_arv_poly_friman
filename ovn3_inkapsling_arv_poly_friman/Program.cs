5f50854ec25d6f9efbcf3a9fff32b2996effc96a
Karl Friman <karl@karlfriman.com>
2021-01-26 10:51:45 +0100

    Add project files.

---
+++ ovn3_inkapsling_arv_poly_friman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ovn3_inkapsling_arv_poly_friman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
